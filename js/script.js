8a3fd07c5eb4a8b949e2937744eb33930d09d2a6
Pedromamede02 <102523813+Pedromamede02@users.noreply.github.com>
2023-10-27 17:38:44 +0100

    first-commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  loginForm.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
